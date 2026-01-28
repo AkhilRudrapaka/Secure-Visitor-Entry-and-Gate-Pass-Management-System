b7641a2728125c1919f471bd1910e6ee2360e5ba
Rudrapaka Akhil <rudrapakaakhil@gmail.com>
2026-01-28 22:57:37 +0530

    Revert Vercel deployment configurations

--- backend/server.js
+++ backend/server.js
@@ -58,10 +58,6 @@ app.use((err, req, res, next) => {
 
 const PORT = process.env.PORT || 5000;
 
-if (require.main === module) {
-    app.listen(PORT, () => {
-        console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
-    });
-}
-
-module.exports = app;
+app.listen(PORT, () => {
+    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
+});
